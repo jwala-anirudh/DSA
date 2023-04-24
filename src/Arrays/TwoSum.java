44fe8f84d8f6985d2279cacf02c4cc20820232f8
Anirudh Jwala <anirudhsai2@gmail.com>
2023-04-24 16:30:15 +0530

    Updated arrays

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -1,26 +1,28 @@
+package Arrays;
+
 public class TwoSum {
 
-  // Brute force
-  static void solution(int[] arr, int target) {
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
-        if (arr[i] + arr[j] == target) {
-          System.out.println(i + " " + j);
-          return;
+    // Brute force
+    static void solution(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
         }
-      }
     }
-  }
 
-  // Optimisation
-  static void opSolution(int[] arr, int target) {
-  }
+    // Optimisation
+    static void opSolution(int[] arr, int target) {
+    }
 
-  public static void main(String[] args) {
-    int[] arr = { 11, 3, 7, 9, 14, 2 };
-    int target = 17;
+    public static void main(String[] args) {
+        int[] arr = {11, 3, 7, 9, 14, 2};
+        int target = 17;
 
-    solution(arr, target);
-    opSolution(arr, target);
-  }
+        solution(arr, target);
+        opSolution(arr, target);
+    }
 }
