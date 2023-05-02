359e27e1390429169d5e6e640eb7125e3972242b
Anirudh Jwala <anirudhsai2@gmail.com>
2023-05-02 21:25:40 +0530

    Leetcode questions

---
+++ src/Arrays/LeetCode75.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class LeetCode75 {
+    public void sortColors(int[] a) {
+        int n = a.length;
+        int low = 0, mid = 0, high = n - 1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (a[mid] == 0) {
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+
+                // Since values are moved to left are 0's they are sorted
+                // Move the pointers to next part of the window
+                low++;
+                mid++;
+            } else if (a[mid] == 1) {
+                // Assuming all 1's are at the correct locations
+                mid++;
+            } else if (a[mid] == 2) {
+                // Occurance of 2's must be swapped with last watch element
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+
+                high--;
+            }
+
+        }
+    }
+}
---
+++ src/Strings/LeetCode28.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class LeetCode28 {
+    public int strStrBruteForce(String haystack, String needle) {
+        if (haystack.contains(needle))
+            return haystack.indexOf(needle);
+        return -1;
+    }
+
+    public int strStr(String haystack, String needle) {
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            } else {
+                i = i - j;
+                j = 0;
+            }
+
+            if (j == needle.length()) {
+                return i - needle.length() + 1;
+            }
+        }
+        return -1;
+    }
+}
