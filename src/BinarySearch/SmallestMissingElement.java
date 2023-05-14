11c2a7285a6688202a7f7029b43ca805f6a206b0
Anirudh Jwala <anirudhsai2@gmail.com>
2023-05-14 20:30:04 +0530

    Binary search leetcode questions

---
+++ src/BinarySearch/SmallestMissingElement.java
@@ -0,0 +1,16 @@
+package BinarySearch;
+
+/**
+ * Find the smallest missing element from a sorted array
+ * <p>
+ * <p>
+ * Given a sorted array of non-negative distinct integers,
+ * find the smallest missing non-negative element in it.
+ * <p>
+ * https://www.techiedelight.com/find-smallest-missing-element-sorted-array/
+ */
+public class SmallestMissingElement {
+    public static void main(String[] args) {
+
+    }
+}
